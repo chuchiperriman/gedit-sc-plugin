20aeec69157e2e62caf7342a335f03f998225423
Jesús Barbero Rodríguez <chuchiperriman@gmail.com>
2009-05-31 12:57:35 +0200

    Created the skeleton of goto provider

---
+++ src/sc-provider-csymbols-goto.c
@@ -0,0 +1,190 @@
+/* 
+ *  sc-provider-csymbols-goto.c - Type here a short description of your plugin
+ *
+ *  Copyright (C) 2008 - perriman
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "sc-provider-csymbols-goto.h"
+#include "sc-symbol.h"
+#include "sc-ctags.h"
+#include <gtksourceview/gtksourcecompletion.h>
+#include <gtksourceview/gtksourcecompletionitem.h>
+
+#define SC_PROVIDER_CSYMBOLS_GOTO_GET_PRIVATE(object)(G_TYPE_INSTANCE_GET_PRIVATE((object), SC_TYPE_PROVIDER_CSYMBOLS_GOTO, ScProviderCsymbolsGotoPrivate))
+
+static void	 sc_provider_csymbols_goto_iface_init	(GtkSourceCompletionProviderIface *iface);
+
+struct _ScProviderCsymbolsGotoPrivate
+{
+	GdkPixbuf *provider_icon;
+	GeditDocument *document;
+};
+
+G_DEFINE_TYPE_WITH_CODE (ScProviderCsymbolsGoto,
+			 sc_provider_csymbols_goto,
+			 G_TYPE_OBJECT,
+			 G_IMPLEMENT_INTERFACE (GTK_TYPE_SOURCE_COMPLETION_PROVIDER,
+				 		sc_provider_csymbols_goto_iface_init))
+
+static const gchar * 
+sc_provider_csymbols_goto_get_name (GtkSourceCompletionProvider *self)
+{
+	return "File Symbols Goto";
+}
+
+static GdkPixbuf * 
+sc_provider_csymbols_goto_get_icon (GtkSourceCompletionProvider *self)
+{
+	return SC_PROVIDER_CSYMBOLS_GOTO (self)->priv->provider_icon;
+}
+
+/* TODO Do better because we load a new pixbuf in all the proposals
+ *      Return the PixBuf that represents the specified type of symbol, or NULL
+ */         
+static GdkPixbuf *
+get_symbol_pixbuf (gchar *type)
+{
+        GdkPixbuf               *pixbuf=NULL;
+        gchar                   *path;
+        GError                  *error = NULL;
+        
+        path = g_strdup_printf (ICON_DIR"/symbol-%s.png", type);
+        
+        if (g_file_test (path, G_FILE_TEST_EXISTS))
+        {
+                pixbuf = gdk_pixbuf_new_from_file(path, &error);
+        }
+        
+        if (error)
+        {
+                g_warning ("Could not load pixbuf: %s\n", error->message);
+                g_error_free(error);
+        }
+        
+        g_free (path);
+        
+        
+        return pixbuf;
+}
+
+static GList *
+sc_provider_csymbols_goto_get_proposals (GtkSourceCompletionProvider *base,
+                                 GtkTextIter                 *iter)
+{
+	ScProviderCsymbolsGoto *self = SC_PROVIDER_CSYMBOLS_GOTO (base);
+	GList *list = NULL, *symbols = NULL, *l;
+	GtkSourceCompletionItem *prop;
+	ScSymbol *s;
+	GdkPixbuf *icon;
+	gchar *info;
+	
+	gchar *uri = gedit_document_get_uri_for_display (self->priv->document);
+	if (g_file_test (uri, G_FILE_TEST_EXISTS))
+	{
+		symbols = sc_ctags_exec_get_symbols (CTAGS_EXEC_FILE, uri);
+	}
+	
+	for (l = symbols; l != NULL; l = g_list_next (l))
+	{
+		s = SC_SYMBOL (l->data);
+		info = NULL;
+		if (s->signature)
+		{
+			info = g_strdup_printf ("<b>%s</b> %s", s->name, s->signature);
+		}
+		icon = get_symbol_pixbuf (s->type);
+		prop = gtk_source_completion_item_new (s->name, s->name, icon, info);
+		list = g_list_append (list, prop);
+		g_free (info);
+		g_object_unref (icon);
+	}
+	
+	return list;
+}
+
+static gboolean
+sc_provider_csymbols_goto_filter_proposal (GtkSourceCompletionProvider *provider,
+                                   GtkSourceCompletionProposal *proposal,
+                                   GtkTextIter                 *iter,
+                                   const gchar                 *criteria)
+{
+	const gchar *label;
+	
+	label = gtk_source_completion_proposal_get_label (proposal);
+	return g_str_has_prefix (label, criteria);
+}
+
+static const gchar *
+sc_provider_csymbols_goto_get_capabilities (GtkSourceCompletionProvider *provider)
+{
+	return GTK_SOURCE_COMPLETION_CAPABILITY_INTERACTIVE ","
+	       GTK_SOURCE_COMPLETION_CAPABILITY_AUTOMATIC;
+}
+
+static void 
+sc_provider_csymbols_goto_finalize (GObject *object)
+{
+	ScProviderCsymbolsGoto *provider = SC_PROVIDER_CSYMBOLS_GOTO (object);
+	
+	if (provider->priv->provider_icon != NULL)
+	{
+		g_object_unref (provider->priv->provider_icon);
+	}
+	
+	G_OBJECT_CLASS (sc_provider_csymbols_goto_parent_class)->finalize (object);
+}
+
+static void 
+sc_provider_csymbols_goto_class_init (ScProviderCsymbolsGotoClass *klass)
+{
+	GObjectClass *object_class = G_OBJECT_CLASS (klass);
+	
+	object_class->finalize = sc_provider_csymbols_goto_finalize;
+	
+	g_type_class_add_private (object_class, sizeof(ScProviderCsymbolsGotoPrivate));
+}
+
+static void
+sc_provider_csymbols_goto_iface_init (GtkSourceCompletionProviderIface *iface)
+{
+	iface->get_name = sc_provider_csymbols_goto_get_name;
+	iface->get_icon = sc_provider_csymbols_goto_get_icon;
+
+	iface->get_proposals = sc_provider_csymbols_goto_get_proposals;
+	iface->filter_proposal = sc_provider_csymbols_goto_filter_proposal;
+	iface->get_capabilities = sc_provider_csymbols_goto_get_capabilities;
+}
+
+static void 
+sc_provider_csymbols_goto_init (ScProviderCsymbolsGoto * self)
+{
+	GtkIconTheme *theme;
+	gint width;
+	
+	self->priv = SC_PROVIDER_CSYMBOLS_GOTO_GET_PRIVATE (self);
+	
+	theme = gtk_icon_theme_get_default ();
+
+	gtk_icon_size_lookup (GTK_ICON_SIZE_MENU, &width, NULL);
+}
+
+ScProviderCsymbolsGoto *
+sc_provider_csymbols_goto_new (GeditDocument *document)
+{
+	ScProviderCsymbolsGoto *self = g_object_new (SC_TYPE_PROVIDER_CSYMBOLS_GOTO, NULL);
+	self->priv->document = document;
+	return self;
+}
---
+++ src/sc-provider-csymbols-goto.h
@@ -0,0 +1,60 @@
+/* 
+ *  sc-provider-csymbols-goto.h - Type here a short description of your plugin
+ *
+ *  Copyright (C) 2008 - perriman
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef __SC_PROVIDER_CSYMBOLS_GOTO_H__
+#define __SC_PROVIDER_CSYMBOLS_GOTO_H__
+
+#include <glib.h>
+#include <glib-object.h>
+#include <gtksourceview/gtksourcecompletionprovider.h>
+#include <gedit/gedit-plugin.h>
+
+G_BEGIN_DECLS
+
+#define SC_TYPE_PROVIDER_CSYMBOLS_GOTO (sc_provider_csymbols_goto_get_type ())
+#define SC_PROVIDER_CSYMBOLS_GOTO(obj) (G_TYPE_CHECK_INSTANCE_CAST ((obj), SC_TYPE_PROVIDER_CSYMBOLS_GOTO, ScProviderCsymbolsGoto))
+#define SC_PROVIDER_CSYMBOLS_GOTO_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST ((klass), SC_TYPE_PROVIDER_CSYMBOLS_GOTO, ScProviderCsymbolsGotoClass))
+#define SC_IS_PROVIDER_CSYMBOLS(obj) (G_TYPE_CHECK_INSTANCE_TYPE ((obj), SC_TYPE_PROVIDER_CSYMBOLS_GOTO))
+#define SC_IS_PROVIDER_CSYMBOLS_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), SC_TYPE_PROVIDER_CSYMBOLS_GOTO))
+#define SC_PROVIDER_CSYMBOLS_GOTO_GET_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS ((obj), SC_TYPE_PROVIDER_CSYMBOLS_GOTO, ScProviderCsymbolsGotoClass))
+
+#define SC_PROVIDER_CSYMBOLS_GOTO_NAME "ScProviderCsymbolsGoto"
+
+typedef struct _ScProviderCsymbolsGoto ScProviderCsymbolsGoto;
+typedef struct _ScProviderCsymbolsGotoPrivate ScProviderCsymbolsGotoPrivate;
+typedef struct _ScProviderCsymbolsGotoClass ScProviderCsymbolsGotoClass;
+
+struct _ScProviderCsymbolsGoto
+{
+	GObject parent;
+	
+	ScProviderCsymbolsGotoPrivate *priv;
+};
+
+struct _ScProviderCsymbolsGotoClass
+{
+	GObjectClass parent;
+};
+
+GType		 sc_provider_csymbols_goto_get_type	(void) G_GNUC_CONST;
+
+ScProviderCsymbolsGoto *sc_provider_csymbols_goto_new (GeditDocument *document);
+
+G_END_DECLS
+
+#endif
